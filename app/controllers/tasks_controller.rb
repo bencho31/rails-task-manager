787231827dda4a56974911811942492621bb58d3
yoann benchetrit <benchetrityoann@gmail.com>
2018-11-12 15:25:49 +0200

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @task.update(tasks_params)
+
+    # redirect to the tasks show page
+    redirect_to tasks_path(@tasks)
+
+  end
+
+    def destroy
+
+    @task.destroy
+
+
+    redirect_to tasks_path
+    end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_tasks
+    @task = Task.find(params[:id])
+  end
+end
